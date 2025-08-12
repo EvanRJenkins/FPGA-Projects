80ebaa2a0884685b1afa94fd6af7e0cf2b4af0bc
Evan Jenkins <JenkinsE@jbu.edu>
2025-08-12 13:44:04 -0500

    Update controller.v

--- verilog_hdl/logic_analyzer/controller.v
+++ verilog_hdl/logic_analyzer/controller.v
@@ -5,6 +5,7 @@ module controller(
   input wire           i_sample_clk,
   input wire           i_tx_ready,
   input wire [11:0]    i_sample_data,
+  input wire           i_capture_cmd,
 
   //Module Outputs
   output reg [11:0]    o_write_data,
@@ -15,28 +16,66 @@ module controller(
 );
 
 // State definitions
-localparam IDLE_STATE      = 2'b00;
-localparam ACTIVE_STATE    = 2'b01;
-localparam TRANSFER_STATE  = 2'b10;
-localparam DONE_STATE      = 2'b11;
+localparam IDLE_STATE       = 2'b00;
+localparam CAPTURE_STATE    = 2'b01;
+localparam TRANSFER_STATE   = 2'b10;
+localparam DONE_STATE       = 2'b11;
 
 // Internal Registers
-reg [1:0] r_last_state;
-reg [1:0] r_next_state;
+reg        r_new_sample;
+reg [1:0]  r_last_state;
+reg [1:0]  r_next_state;
 reg [11:0] r_sample_latch;
 
 always @(posedge i_sample_clk)
   begin
     r_sample_latch <= i_sample_data;
+    r_new_sample <= 1'b1;
   end
 
   
 always @(posedge i_clk)
   begin
     
+    case (r_next_state):
+
+      IDLE_STATE:
+        begin
+          if (r_last_state == DONE_STATE)
+            begin
+              // Reset everything
+            end
+          if (i_capture_cmd)
+            begin
+              r_next_state <= CAPTURE_STATE;
+            end
+        end
+
+      CAPTURE_STATE:
+        begin
+          if (r_new_sample == 1)
+            begin
+              if (o_write_address < 
+              o_write_data <= r_sample_latch;
+              o_write_address <= o_write_address + 'b1;
+              r_new_sample <= 1'b0;
+              
+            end
+        end
+
+      TRANSFER_STATE:
+        begin
+        end
+
+      DONE_STATE:
+        begin
+        end
+
+    endcase
   end
 
 
 
 endmodule
+
 
